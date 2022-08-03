5fdb0860e4d44995b2558135f107b60b5420de2a
James Jiang <james.yuhaojiang@gmail.com>
2022-08-03 16:43:24 -0700

    Header style

--- frontend/src/pages/ui/Footer.tsx
+++ frontend/src/pages/ui/Footer.tsx
@@ -3,9 +3,9 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 export default function Footer() {
-  return (
-    <div className="footer text-center w-100 ml-0">
-      <p className="copyright">Created by James Jiang @{new Date().getFullYear()}</p>
-    </div>
-  );
+    return (
+        <div className="footer text-center w-100 ml-0">
+            <p className="copyright">Created by James Jiang @{new Date().getFullYear()}</p>
+        </div>
+    );
 }
