990dc6602287865258e53dee926a352171f8327f
James Jiang <james.yuhaojiang@gmail.com>
2022-08-03 16:26:33 -0700

    Added sass, header, footer page

---
+++ frontend/src/pages/ui/Footer.tsx
@@ -0,0 +1,11 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+export default function Footer() {
+  return (
+    <div className="footer text-center w-100 ml-0">
+      <p className="copyright">Created by James Jiang @{new Date().getFullYear()}</p>
+    </div>
+  );
+}
